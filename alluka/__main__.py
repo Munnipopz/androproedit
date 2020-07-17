3a35d147ab91f85efde1a6ffd9a4c404a172def0
VILLAIN <64843897+DONOFTELEGRAM@users.noreply.github.com>
2020-07-17 08:12:46 +0530

    Update __main__.py

--- alluka/__main__.py
+++ alluka/__main__.py
@@ -61,7 +61,7 @@ def vercheck() -> str:
 
 
 SOURCE_STRING = """
-⚡I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [here](https://github.com/Sur-vivor/CinderellaProBot)
+⚡I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [here](https://github.com/DONOFTELEGRAM/AndreaProBot)
 ⚡You Can Clone Me [Here](https://heroku.com/deploy?template=https://github.com/DONOFTELEGRAM/AndreaProBot.git)
 """
 
