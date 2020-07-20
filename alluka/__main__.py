23eac5f1bb1ea64ca6b626c28e4f2b3c6ce16185
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2020-07-20 14:19:32 +0530

    Update __main__.py

--- alluka/__main__.py
+++ alluka/__main__.py
@@ -81,7 +81,7 @@ GDPR = []
 
 START_IMG = os.environ.get('START_IMG', None)
 if START_IMG is None:
-    img = "https://telegra.ph/file/511ad504656e712b88235.jpg"
+    img = "https://telegra.ph/file/4d218fef44058c120bedd.jpg"
 else:
   img = START_IMG    
     
